0e0ce4f62f18d6a572ed497d2d891a2e75fc92cd
pangaea1234 <87812833+pangaea1234@users.noreply.github.com>
2023-01-30 20:51:05 +0530

    Add files via upload

---
+++ for practice/basic/6.cpp
@@ -0,0 +1,10 @@
+//concatination
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string Firstname="Pangaea ";
+    string Lastname="Lal";
+    string Fullname = Firstname + Lastname;
+    cout<< Fullname <<endl;
+    return 0;
+}
