ba615bc99656c6bdabd6e35cdfba991327763f15
pangaea1234 <87812833+pangaea1234@users.noreply.github.com>
2023-01-30 20:47:18 +0530

    Add files via upload

---
+++ practice/13.cpp
@@ -0,0 +1,11 @@
+//while loop
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int i=0;
+    while(i<=10) {
+        cout<<i<<endl;
+        i=i+1;//we can also use i+=1 or i++ here
+    }
+    return 0;
+}
