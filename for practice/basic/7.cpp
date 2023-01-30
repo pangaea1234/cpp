d07df4870e96e38882023c309e6e5c303219fd70
pangaea1234 <87812833+pangaea1234@users.noreply.github.com>
2023-01-30 20:53:28 +0530

    Add files via upload

---
+++ for practice/basic/7.cpp
@@ -0,0 +1,10 @@
+//append function
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string Firstname="Pangaea ";
+    string Lastname="Lal";
+    string a = Firstname.append(Lastname); //append is to add, here add firstname in lastname
+    cout<< a <<endl;
+    return 0;
+}
