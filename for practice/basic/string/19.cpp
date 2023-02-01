b6f6e5094a1de7d55ede950e8ece155d017fa15b
pangaea1234 <87812833+pangaea1234@users.noreply.github.com>
2023-02-01 15:57:00 +0530

    Add files via upload

---
+++ for practice/basic/string/19.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the txt string is: " << txt.length()<<endl;//length and size both are same
+    cout << "The length of the txt string is: " << txt.size()<<endl;
+    return 0;
+}
