8b4ebd039910366bf31de42045591531de38ee5f
pangaea1234 <87812833+pangaea1234@users.noreply.github.com>
2023-02-01 19:46:29 +0530

    Add files via upload

---
+++ for practice/basic/24.cpp
@@ -0,0 +1,11 @@
+//boolean expression 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x = 10;
+    int y = 9;
+    cout << (x > y)<<endl; // returns 1 (true), because 10 is higher than 9
+    cout << (x == 11)<<endl; // returns 0 (false), because 11 is not equal 10
+
+    return 0;
+}
