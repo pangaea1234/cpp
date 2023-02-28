9fccfd5ece66ec0072e7a0245942c4c4c6935c0f
Pangaea Swarnim Lal <87812833+pangaea1234@users.noreply.github.com>
2023-02-28 23:51:53 +0530

    Add files via upload

---
+++ for practice/Array/31.cpp
@@ -0,0 +1,18 @@
+//loops and array
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+    for (int i = 0; i < 5; i++)
+     {
+        cout << cars[i] << "\n";
+     }
+     int myNumbers[5] = {10, 20, 30, 40, 50};
+for (int j = 0; j < 5; j++) {
+  cout << myNumbers[j] << "\n";
+}
+
+    return 0;
+
+}
