b13ba32cff3ba58c72db3f51cfef6a5b8d89dcda
Pangaea Swarnim Lal <87812833+pangaea1234@users.noreply.github.com>
2023-02-28 16:57:07 +0530

    Add files via upload

---
+++ for practice/Array/29.cpp
@@ -0,0 +1,10 @@
+/* Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.*/
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string  makeup[5] = {"Primer","conceler","color corrector","Foundation","Eye shaadow"};
+    cout<<makeup[0]<<endl;
+    int num[4]={10,20,30,40};
+    cout<<num[1]<<endl;
+    return 0;
+}
