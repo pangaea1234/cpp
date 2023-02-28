5064242efcbd8aca50dd45d68379ba8f31f2a36c
Pangaea Swarnim Lal <87812833+pangaea1234@users.noreply.github.com>
2023-02-28 16:55:41 +0530

    Add files via upload

---
+++ for practice/loops/28.cpp
@@ -0,0 +1,12 @@
+//continue 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    for (int i = 0; i < 10; i++) {
+  if (i == 4) {
+    continue;
+  }
+  cout << i << "\n";
+}
+    return 0;
+}
